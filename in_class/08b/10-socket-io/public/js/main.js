ff69b6238eaa5b68e13a5f9eda7b239af848bbaa
Michael Musick <michael@michaelmusick.com>
2017-04-06 10:57:35 -0600

    app

--- in_class/08b/10-socket-io/public/js/main.js
+++ in_class/08b/10-socket-io/public/js/main.js
@@ -3,7 +3,7 @@
 // In our current case, its a localhost
 var socket = io("http://localhost:3000");
 
-// disconnect event
+// dosconnect event
 socket.on("disconnect", function() {
 	setTitle("Disconnected");
 });
