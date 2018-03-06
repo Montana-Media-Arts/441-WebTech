2360a444a87cc6868d88dca4f4c1875ede29adda
Michael Musick <m.a.musick@gmail.com>
2018-03-06 16:11:46 -0700

    example 3

--- lecture_code/06/02_idSelector_03/script.js
+++ lecture_code/06/02_idSelector_03/script.js
@@ -1,2 +1,5 @@
 let node = document.getElementById('d1');
-node.innerText = "hey there";
+node.innerText = "<h1>This does not work</h1>";
+
+node = document.getElementById('d2');
+node.innerHTML = `<div style='background:#ff0000;'><h1>This Works!</h1><p>SO MUCH FUN</p></div>`;
