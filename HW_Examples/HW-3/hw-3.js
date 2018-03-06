1df428bf65760cef32fefe5e9e550fc32d0dccc0
Michael Musick <m.a.musick@gmail.com>
2018-03-06 12:20:50 -0700

    ch-ch-ch-changes

--- HW_Examples/HW-3/hw-3.js
+++ HW_Examples/HW-3/hw-3.js
@@ -55,7 +55,7 @@ the correct output occurs.
 
 
 /**************************************************/
-// PROBLEM 2 - FizzBuzz
+// PROBLEM 3 - FizzBuzz
 //
 // Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions.
 // For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
