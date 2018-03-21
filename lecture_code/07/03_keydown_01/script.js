fbab4ecb385c7ce9cc6412db4d7a050d0792ab09
Michael Musick <m.a.musick@gmail.com>
2018-03-21 12:19:12 -0600

    hmmmm

--- lecture_code/07/03_keydown_01/script.js
+++ lecture_code/07/03_keydown_01/script.js
@@ -1,13 +1,21 @@
 document.addEventListener("keydown", printKey);
-
+var der;
 function printKey( event ) {
-    let el = document.querySelector('div');
+    let el = document.querySelector('#key-capture');
 
     // get the specific key pressed
     let key = event.key;
 
+    der = event;
+
     // if the key is a letter, then add it to the current div element
-    if( (key.charCodeAt(0) > 64 && key.charCodeAt(0) < 91) || (key.charCodeAt(0) > 96 && key.charCodeAt(0) < 123) ) {
+    if(
+        (
+            (key.charCodeAt(0) > 64 && key.charCodeAt(0) < 91) ||
+            (key.charCodeAt(0) > 96 && key.charCodeAt(0) < 123)
+        ) && key.length === 1 )
+    {
+        console.log(key.charCodeAt(0))
         el.innerText = el.innerText + key;
     }
 
