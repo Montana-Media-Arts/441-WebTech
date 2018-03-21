3826828115ccb1ae4846c98dce81df732811e97f
Michael Musick <m.a.musick@gmail.com>
2018-03-21 17:00:19 -0600

    small fix

--- lecture_code/07/04_forms_02/script.js
+++ lecture_code/07/04_forms_02/script.js
@@ -1,10 +1,13 @@
 let bgColRads = document.querySelectorAll(".bgColor");
 let formElem = document.querySelector("form");
 
+// register an event listener
 formElem.addEventListener("click", changeBG);
 
-function changeBG(e){
-    for( rad of bgColRads ){
+// work through each o the elements.
+// Use the value of the check element to set the bg color
+function changeBG(){
+    for( let rad of bgColRads ){
         if( rad.checked ){
             document.body.style.background = rad.value;
         }
