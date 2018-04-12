39800f6119acd68398fd572143c40275d6b852d3
Michael Musick <m.a.musick@gmail.com>
2018-04-12 13:19:43 -0600

    updates

--- HW_Examples/HW-5/hw-5.js
+++ HW_Examples/HW-5/hw-5.js
@@ -41,11 +41,11 @@ console.log(new Vec(3, 4).length);
 //
 // The standard JavaScript environment provides another data structure called Set. Like an instance of Map, a set holds a collection of values. Unlike Map, it does not associate other values with those—it just tracks which values are part of the set. A value can only be part of a set once—adding it again doesn’t have any effect.
 //
-// Write a class called Group (since Set is already taken). Like Set, it has add, delete, and has methods. Its constructor creates an empty group, add adds a value to the group (but only if it isn’t already a member), delete removes its argument from the group (it if was a member), and has returns a Boolean value indicating whether its argument is a member of the group.
+// Write a class called Group (since Set is already taken). Like Set, it has add, delete, and has methods. Its constructor creates an empty group. "add" adds a value to the group (but only if it isn’t already a member). "delete" removes its argument from the group (it if was a member). "has" returns a Boolean value indicating whether its argument is a member of the group.
 //
 // Use the === operator, or something equivalent such as indexOf, to determine whether two values are the same.
 //
-// Give the class a static from method that takes an iteratable object as argument and creates a group that contains all the values produced by iterating over it.
+// Give the class a static "from" method that takes an iteratable object as argument and creates a group that contains all the values produced by iterating over it.
 
 class Group {
   // Your code here.
